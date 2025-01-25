ac68e320180eaeeea40b1b3736cbbaf77f83933e
shimarda <ryo1031shima@gmail.com>
2025-01-25 06:29:51 +0000

    init commit

---
+++ src/tshark-2024.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# JSTの時間範囲を指定
+start_jst=$1 # 開始時間
+end_jst=$2   # 終了時間
+
+# JSTからUTCに変換
+start_utc=$(date -u -d "$(echo $start_jst | sed -r 's/([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/\1-\2-\3 \4:\5/') JST" +"%Y%m%d%H%M")
+end_utc=$(date -u -d "$(echo $end_jst | sed -r 's/([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/\1-\2-\3 \4:\5/') JST" +"%Y%m%d%H%M")
+
+# 元の圧縮ファイルのパスを取得
+original_files=$(ls -1 /mnt/qnap2/dnscap/2024/dump-*.gz | sort)
+
+function convert_time_to_jst() {
+    # 引数からファイル名を取得
+    file_name="$1"
+ 
+    # ファイル名から時刻部分を取得
+    time_part=$(echo "$file_name" | grep -oP '\d{12}')
+   
+    # UTCからJSTに変換
+    jst_time=$(TZ=Asia/Tokyo date --date "${time_part:0:4}-${time_part:4:2}-${time_part:6:2} ${time_part:8:2}:${time_part:10:2}:00 UTC" '+%Y%m%d%H%M')
+    echo $jst_time
+}
+
+# 各ファイルに対して処理を実行
+for file in $original_files
+do
+    # ファイル名からUTC時刻部分を抽出
+    file_time=$(basename "$file" | grep -oP '\d{12}')
+
+    # ファイルの時刻が指定範囲内か確認
+    if [[ "$file_time" -ge "$start_utc" && "$file_time" -le "$end_utc" ]]; then
+        # コピー先のディレクトリ
+        dst_dir="/mnt/qnap2/shimada/input/"
+        copied_file="$dst_dir$(basename "$file")"
+        
+        # ファイルをコピー
+        cp "$file" "$dst_dir"
+
+        # 解凍するためのファイルパス（解凍後のファイル名）
+        input_file="${copied_file%.gz}"
+
+        # 解凍
+        gzip -d -k "$copied_file"
+
+        # 元のファイル名からUTC時刻部分を抽出し、JSTに変換
+        jst_timestamp=$(convert_time_to_jst "$input_file")
+        time="${jst_timestamp:0:4}-${jst_timestamp:4:2}-${jst_timestamp:6:2}-${jst_timestamp:8:2}"
+
+        # 出力ファイル名を生成
+        output_file="${dst_dir}${time}.csv"
+
+        # tsharkコマンドを実行し、結果をCSV形式で出力
+        tshark -r "$input_file" -Y "(ip.src == 130.158.68.20 or ip.src == 130.158.68.21 or ip.src == 130.158.71.54) and (dns.flags.authoritative == 1) and (dns.flags.rcode == 0)" -T fields -e frame.time -e ip.src -e ip.dst -e ipv6.dst -e dns.qry.name -e dns.qry.type -E header=y -E separator=, -E quote=d > "$output_file"
+
+        # 解凍ファイルを削除（不要な場合）
+        rm "$input_file"
+        rm "$copied_file"
+
+        # 出力ファイルのパスを表示
+        echo "$output_file"
+    fi
+done
---
+++ src/tshark-args.py
@@ -0,0 +1,146 @@
+import re
+import pandas as pd
+import os
+import glob
+import csv
+import math
+import operator
+import argparse
+
+# ファイル名からファイルを開く
+def open_reader(file_name):
+    #権威
+    file_path = f"/mnt/qnap2/shimada/input/{file_name}"
+    df = pd.read_csv(file_path)
+    return df
+
+# パターンに合うファイル名リスト
+def file_lst(year, month, day):
+    #権威側
+    path = "/mnt/qnap2/shimada/input/*.csv"
+
+    pattern = re.compile(rf"{year}-{month}-{day}-\d{{2}}\.csv")
+    #files = os.listdir(path)
+    files = sorted(glob.glob(path))
+    filtered_files = [file for file in files if pattern.match(os.path.basename(file))]
+
+    return filtered_files
+
+# ファイル名リストから時間のみを抽出
+def file_time(file_lst):
+    time_list = [
+        os.path.basename(f).replace('-', '').replace('.csv', '') for f in file_lst
+    ]
+    return time_list
+
+# 一意にしてソートされたリストを作成
+def sort_lst(reader, lst):
+    exist_set = set(lst)
+    new_items = {row[0] for row in reader if row[0] not in exist_set}
+
+    lst.extend(new_items)
+    lst = sorted(lst, key=operator.itemgetter(0))
+    return lst
+
+# サブドメインを抽出する関数
+def extract_subdomain(qname):
+    suffix = '.tsukuba.ac.jp'
+
+    if isinstance(qname, str):
+        qname_lower = qname.lower()
+        if qname_lower.endswith(suffix):
+            # サフィックスを取り除く
+            qname_no_suffix = qname_lower[:-len(suffix)]
+            if qname_no_suffix.endswith('.'):
+                qname_no_suffix = qname_no_suffix[:-1]
+            if qname_no_suffix:
+                # ドットで分割し、最後の要素を取得
+                subdomain = qname_no_suffix.split('.')[-1]
+                return subdomain
+    return None
+
+
+#権威サーバーからの応答を使用するため
+#カウントするIPアドレスは送信先IPアドレスを用いる
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-y', help='year')
+    parser.add_argument('-m', help='month')
+    parser.add_argument('-d', help='day')
+
+    args = parser.parse_args()
+
+    year = args.y
+    month = args.m
+    day = args.d
+
+    # パターンにあうファイルの時間をリストへ
+    r = file_lst(year, month, day)
+    time_lst = file_time(r)
+    # keyに日にち、値に時間
+    file_dict = {}
+
+    # 日にちごとに時間リストへ入れる
+    for time in time_lst:
+        month = time[4:6]
+        day = time[6:8]
+        hour = time[8:10]
+        if day not in file_dict.keys():
+            file_dict[day] = []
+        file_dict[day].append(hour)
+
+    for day in file_dict.keys():
+        uni_src_set = set()
+        domain_dict = {}
+        A_tot = 0
+
+        #1時間ごとにファイルにアクセス
+        for hour in file_dict[day]:
+            print(month + day + hour)
+
+            #データフレームを読み込む
+            input_file_name = f"{year}-{month}-{day}-{hour}.csv"
+            df = open_reader(input_file_name)
+
+            #'src_addr'のユニークなセットを更新A_total
+            uni_src_set.update(df['ip.dst'].unique())
+
+            #'qname'からサブドメインを抽出して新しい列を追加
+            df['subdomain'] = df['dns.qry.name'].apply(extract_subdomain)
+
+            #サブドメインが存在する行のみ
+            df_sub = df[df['subdomain'].notnull()]
+
+            #'subdomain'ごとにグループ化して'src_addr'のセットを取得
+            hour_domain_src_addr_dict = df_sub.groupby('subdomain')['ip.dst'].apply(set).to_dict()
+
+            # 結果を更新
+            for domain, src_addrs in hour_domain_src_addr_dict.items():
+                if domain in domain_dict:
+                    domain_dict[domain].update(src_addrs)
+                else:
+                    domain_dict[domain] = src_addrs
+
+        A_tot = len(uni_src_set)
+
+        # マグニチュードの計算とソート
+        domain_magnitude_list = []
+        magnitude_dict = {}
+        for key in domain_dict.keys():
+            src_addr_count = len(domain_dict[key])
+            if src_addr_count > 0:
+                magnitude = 10 * math.log(src_addr_count) / math.log(A_tot)
+                magnitude_dict[key] = magnitude
+
+        # マグニチュードの降順でソート
+        mag_dict = dict(sorted(magnitude_dict.items(), key=lambda item: item[1], reverse=True))
+
+        # 結果をCSVファイルに書き込む
+        csv_file_path = f"/home/shimada/analysis/output/dns_mag/{year}-{month}-{day}.csv"
+        with open(csv_file_path, "a", newline='') as f:
+            writer = csv.writer(f, delimiter=',')
+            writer.writerow(['day', 'domain', 'dnsmagnitude'])
+            for subdomain in mag_dict:
+                writer.writerow([f"{day}", subdomain, str(mag_dict[subdomain])])
